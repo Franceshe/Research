3afbd488434e4017b3163d4212ef89e8ff2b85a8
Frances He <hesiyun1996@gmail.com>
2020-09-02 22:26:07 +0800

    add: priceRange and price to dimention

--- data_visualization/d3-dashboard/schema/Orders.js
+++ data_visualization/d3-dashboard/schema/Orders.js
@@ -1,31 +1,77 @@
 cube(`Orders`, {
-  sql: `
-  select 1 as id, 100 as amount, 'new' status
-  UNION ALL
-  select 2 as id, 200 as amount, 'new' status
-  UNION ALL
-  select 3 as id, 300 as amount, 'processed' status
-  UNION ALL
-  select 4 as id, 500 as amount, 'processed' status
-  UNION ALL
-  select 5 as id, 600 as amount, 'shipped' status
-  `,
-
+  sql: `SELECT * FROM public.orders`,
+  
+  joins: {
+    Products: {
+      sql: `${CUBE}.product_id = ${Products}.id`,
+      relationship: `belongsTo`
+    },
+    
+    Users: {
+      sql: `${CUBE}.user_id = ${Users}.id`,
+      relationship: `belongsTo`
+    }
+  },
+  
   measures: {
     count: {
-      type: `count`
+      type: `count`,
+      drillMembers: [id, createdAt]
     },
 
-    totalAmount: {
-      sql: `amount`,
+      
+    number: {
+      sql: `number`,
       type: `sum`
     }
   },
-
+  
   dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true
+    },
+
+        // The following `dimention` is added to Orders Cube
+    // as subQuery which gives the sum of all line items
+    // that belong to that order
+    price:  {
+      sql: `${LineItems.price}`,	  
+      subQuery: true,
+      type:`number`,
+      format: `currency`
+    },
+
+    //add the following `priceRange` dimention by using a
+    // case statement to define a conditional logic for price
+    // buckets
+    priceRange: {
+	    type: `string`,
+	    case: {
+	        when: [
+		    {sql: `${price} < 101`, label: `$0 - $100`},
+		    {sql: `${price} < 201`, label: `$100 - $200`}
+	        ],
+	        else:{
+		  label: `$200+`
+	      }
+	     },
+      },
+    
     status: {
       sql: `status`,
       type: `string`
+    },
+    
+    createdAt: {
+      sql: `created_at`,
+      type: `time`
+    },
+    
+    completedAt: {
+      sql: `completed_at`,
+      type: `time`
     }
   }
 });
